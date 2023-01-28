f2c3ea19c759f6d66a06cd3d091a6e91f1d751ff
Ammar Mohieldin <ammarmo@my.yorku.ca>
2023-01-28 13:03:24 -0500

    linked map.js to main.html

--- maps.py
+++ maps.py
@@ -1,1 +1,13 @@
-print("this is where the maps api will be implemented")
+import googlemaps
+from datetime import datetime
+import folium
+
+# Create a map object centered on New York City
+nyc_map = folium.Map(location=[40.693943, -73.985880], zoom_start=13)
+
+# Save the map to an HTML file
+nyc_map.save('nyc_map.html')
+
+nyc_map = folium.Map(location=[40.693943, -73.985880], zoom_start=13, api_key='YOUR_API_KEY')
+
+<iframe src="nyc_map.html" width="600" height="450"></iframe>
