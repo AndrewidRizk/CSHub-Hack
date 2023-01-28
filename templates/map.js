c0c364528fafc84a7df1d97415e7a94d1bd517b4
Ammar Mohieldin <ammarmo@my.yorku.ca>
2023-01-28 14:34:48 -0500

    Added location change feature to map

--- templates/map.js
+++ templates/map.js
@@ -2,13 +2,39 @@ var map;
 var service;
 var infowindow;
 
-function initialize() {
+async function initialize() {
     var pyrmont = new google.maps.LatLng(-33.8665433, 151.1956316);
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: pyrmont,
-        zoom: 15
+        zoom: 18
     })
+
+    geocoder = new google.maps.Geocoder(); 
+    geocode({ address: "Taj Mall" })
 }
+
+function geocode(request) {
+    var coords = [];
+    geocoder
+      .geocode(request)
+      .then((result) => {
+        const { results } = result;
+  
+        map.setCenter(results[0].geometry.location);
+        coords[0] = results[0].geometry.location.lat();
+        coords[1] = results[0].geometry.location.lng();
+        console.log(coords)
+        marker1 = new google.maps.Marker({
+            map,
+            draggable: false,
+            position: { lat: coords[0], lng: coords[1] },
+        });
+        
+      })
+      .catch((e) => {
+        alert("Geocode was not successful for the following reason: " + e);
+      });
+ }
 
 google.maps.event.addDomListener(window, 'load', initialize)
