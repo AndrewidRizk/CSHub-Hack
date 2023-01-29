a51986fd5e80e939b9b1ea0c592b24eb5b83112c
Ammar Mohieldin <ammarmo@my.yorku.ca>
2023-01-28 20:49:05 -0500

    final commit

---
+++ templates/test.js
@@ -0,0 +1,3 @@
+var campusevent = "(Preparing for Career Fair Success) Job Search and Networking Strategies webinar, 2023-01-13, York University, 10:00am ";
+const myArray = campusevent.split(",");
+console.log(myArray[2])
